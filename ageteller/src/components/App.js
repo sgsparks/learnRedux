1708f729abffd6954ed85bdee27e72a40d48e65a
sam <sparks>
2021-07-20 14:18:28 -0700

    ageTeller

---
+++ ageteller/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form inline>
+          <h2>Input Your Birthday!</h2>
+          <FormControl type='date'>
+          </FormControl>
+          {' '}
+          <Button>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default App;
