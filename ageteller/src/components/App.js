66a93c6bceeeee1790678379d48f8ffa83ae4927
sam <sparks>
2021-07-20 15:04:34 -0700

    complete

--- ageteller/src/components/App.js
+++ ageteller/src/components/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 import './App.css'
+import AgeStats from './AgeStats'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      birthday: '1992-06-21',
+      showStats: false
+    }
+  }
+
+  changeBirthday() {
+    this.setState({
+      birthday: this.state.newDate,
+      showStats: true
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
         <Form inline>
           <h2>Input Your Birthday!</h2>
-          <FormControl type='date'>
+          <FormControl
+            type='date'
+            onChange={event => this.setState({ newDate: event.target.value })}
+          >
           </FormControl>
           {' '}
-          <Button>
+          <Button onClick={() => this.changeBirthday()}>
             Submit
           </Button>
+          {
+            this.state.showStats ?
+              <AgeStats date={this.state.birthday} />
+              : <div />
+          }
+
         </Form>
       </div>
     )
