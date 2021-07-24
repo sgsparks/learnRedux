79b232e50e09f281ecb45b188e6eb546b4bbcdbb
sam <sparks>
2021-07-24 11:30:51 -0700

    finished

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import ReactDom from 'react-dom'
+function isMerge(s, part1, part2) {
+  let combined = part1 + part2;
+  let data = {};
+  for (let i = 0; i < combined.length; i++) {
+    if (!data[combined[i]) {
+      data[combined[i]] = true
+    }
+  }
+  for (let j = 0; j < s.length; j++) {
+    if (!data[s[j]) return false;
+  }
+  return true;
+}
